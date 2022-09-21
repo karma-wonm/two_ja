8ae4065cdf052666cb6a6b7f5390b4049de5c71a
Nastya Sizykh <sns0299@gmail.com>
2022-09-21 16:34:24 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int price, priceMile= 20, mile;
+        int price, priceMile = 20, mile;
         System.out.println("Введите стоимость билета для вычисления миль, которые будут за него начислены");
-
         Scanner scanner = new Scanner(System.in);
         price = scanner.nextInt();
-        mile= price / priceMile;
+        mile = price / priceMile;
         System.out.println("За купленный билет будет начисленно количество миль равное " + mile);
     }
 }
